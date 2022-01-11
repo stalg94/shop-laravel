298beec5291895036fc6f3ea6b98b04de82031bb
stalg94 <stalg28@gmail.com>
2022-01-11 20:12:47 +0200

    Как пользоваться библиотекой darryldecode/laravelshoppingcart

--- resources/views/pet-shop/layout.blade.php
+++ resources/views/pet-shop/layout.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Marten - Pet Food eCommerce Bootstrap4 Template</title>
+        <title>Pet Food Magazine</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Favicon -->
@@ -28,7 +28,7 @@
                     <div class="row">
                         <div class="col-lg-4 col-md-4 col-12">
                             <div class="welcome-area">
-                                <p>Default welcome msg! </p>
+                                <p>Welcome To Our Store! </p>
                             </div>
                         </div>
                         <div class="col-lg-8 col-md-8 col-12">
@@ -61,110 +61,15 @@
                     <div class="row">
                         <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-5">
                             <div class="logo pt-39">
-                                <a href="index."><img alt="" src="img/logo/logo.png"></a>
+                                <a href="{{route('home')}}"><img alt="" src="img/logo/logo.png"></a>
                             </div>
                         </div>
                         <div class="col-xl-8 col-lg-7 d-none d-lg-block">
                             <div class="main-menu text-center">
                                 <nav>
                                     <ul>
-                                        <li><a href="{{route('home')}}">HOME</a>
-                                            <ul class="submenu">
-                                                <li>
-                                                    <a href="index.">home version 1</a>
-                                                </li>
-                                                <li>
-                                                    <a href="index-2.">home version 2</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="mega-menu-position"><a href="{{route('food')}}">Food</a>
-                                            <ul class="mega-menu">
-                                                <li>
-                                                    <ul>
-                                                        <li class="mega-menu-title">Dogs Food</li>
-                                                        <li><a href="shop-page.">Eggs</a></li>
-                                                        <li><a href="shop-page.">Carrots</a></li>
-                                                        <li><a href="shop-page.">Salmon fishs</a></li>
-                                                        <li><a href="shop-page.">Peanut Butter</a></li>
-                                                        <li><a href="shop-page.">Grapes & Raisins</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li>
-                                                    <ul>
-                                                        <li class="mega-menu-title">Cats Food</li>
-                                                        <li><a href="shop-page.">Meat</a></li>
-                                                        <li><a href="shop-page.">Fish</a></li>
-                                                        <li><a href="shop-page.">Eggs</a></li>
-                                                        <li><a href="shop-page.">Veggies</a></li>
-                                                        <li><a href="shop-page.">Cheese</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li>
-                                                    <ul>
-                                                        <li class="mega-menu-title">Fishs Food</li>
-                                                        <li><a href="shop-page.">Rice</a></li>
-                                                        <li><a href="shop-page.">Veggies</a></li>
-                                                        <li><a href="shop-page.">Cheese</a></li>
-                                                        <li><a href="shop-page.">wheat bran</a></li>
-                                                        <li><a href="shop-page.">Cultivation</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li>
-                                                    <ul>
-                                                        <li><a href="shop-page."><img alt="" src="img/banner/menu-img-4.jpg"></a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">PAGES</a>
-                                            <ul class="submenu">
-                                                <li>
-                                                    <a href="about-us.">about us</a>
-                                                </li>
-                                                <li>
-                                                    <a href="shop-page.">shop page</a>
-                                                </li>
-                                                <li>
-                                                    <a href="shop-list.">shop list</a>
-                                                </li>
-                                                <li>
-                                                    <a href="product-details.">product details</a>
-                                                </li>
-                                                <li>
-                                                    <a href="cart.">cart page</a>
-                                                </li>
-                                                <li>
-                                                    <a href="checkout.">checkout</a>
-                                                </li>
-                                                <li>
-                                                    <a href="wishlist.">wishlist</a>
-                                                </li>
-                                                <li>
-                                                    <a href="contact.">contact us</a>
-                                                </li>
-                                                <li>
-                                                    <a href="my-account.">my account</a>
-                                                </li>
-                                                <li>
-                                                    <a href="login-register.">login / register</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="blog-leftsidebar.">Blog</a>
-                                            <ul class="submenu">
-                                                <li>
-                                                    <a href="blog.">blog page</a>
-                                                </li>
-                                                <li>
-                                                    <a href="blog-leftsidebar.">blog left sidebar</a>
-                                                </li>
-                                                <li>
-                                                    <a href="blog-details.">blog details</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        {{-- <li><a href="{{route('about-us')}}">ABOUT</a></li> --}}
+                                        <li><a href="{{route('home')}}">HOME</a></li>
+                                        <li><a href="{{route('food')}}">Food</a>
                                         <li><a href="{{route('about')}}">ABOUT</a></li>
                                         <li><a href="{{route('contact-us')}}">CONTACT US</a></li>
                                     </ul>
@@ -187,146 +92,6 @@
                                         </form>
                                     </div>
                                 </div>
-                                <div class="header-login same-style">
-                                    <a href="login-register."><i class="icon-user icons"></i></a>
-                                </div>
-                                <div class="header-cart same-style">
-                                    <button class="icon-cart">
-                                        <i class="icon-handbag"></i>
-                                        <span class="count-style">02</span>
-                                    </button>
-                                    <div class="shopping-cart-content">
-                                        <ul>
-                                            <li class="single-shopping-cart">
-                                                <div class="shopping-cart-img">
-                                                    <a href="#"><img alt="" src="img/cart/cart-1.jpg"></a>
-                                                </div>
-                                                <div class="shopping-cart-title">
-                                                    <h4><a href="#">Dog Calcium Food </a></h4>
-                                                    <h6>Qty: 02</h6>
-                                                    <span>$260.00</span>
-                                                </div>
-                                                <div class="shopping-cart-delete">
-                                                    <a href="#"><i class="ti-close"></i></a>
-                                                </div>
-                                            </li>
-                                            <li class="single-shopping-cart">
-                                                <div class="shopping-cart-img">
-                                                    <a href="#"><img alt="" src="img/cart/cart-2.jpg"></a>
-                                                </div>
-                                                <div class="shopping-cart-title">
-                                                    <h4><a href="#">Dog Calcium Food</a></h4>
-                                                    <h6>Qty: 02</h6>
-                                                    <span>$260.00</span>
-                                                </div>
-                                                <div class="shopping-cart-delete">
-                                                    <a href="#"><i class="ti-close"></i></a>
-                                                </div>
-                                            </li>
-                                        </ul>
-                                        <div class="shopping-cart-total">
-                                            <h4>Shipping : <span>$20.00</span></h4>
-                                            <h4>Total : <span class="shop-total">$260.00</span></h4>
-                                        </div>
-                                        <div class="shopping-cart-btn">
-                                            <a href="cart.">view cart</a>
-                                            <a href="checkout.">checkout</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="mobile-menu-area electro-menu d-md-block col-md-12 col-lg-12 col-12 d-lg-none d-xl-none">
-                            <div class="mobile-menu">
-                                <nav id="mobile-menu-active">
-                                    <ul class="menu-overflow">
-                                        <li><a href="#">HOME</a>
-                                            <ul>
-                                                <li><a href="index.">home version 1</a></li>
-                                                <li><a href="index-2.">home version 2</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">pages</a>
-                                            <ul>
-                                                <li>
-                                                    <a href="about-us.">about us</a>
-                                                </li>
-                                                <li>
-                                                    <a href="shop-page.">shop page</a>
-                                                </li>
-                                                <li>
-                                                    <a href="shop-list.">shop list</a>
-                                                </li>
-                                                <li>
-                                                    <a href="product-details.">product details</a>
-                                                </li>
-                                                <li>
-                                                    <a href="cart.">cart page</a>
-                                                </li>
-                                                <li>
-                                                    <a href="checkout.">checkout</a>
-                                                </li>
-                                                <li>
-                                                    <a href="wishlist.">wishlist</a>
-                                                </li>
-                                                <li>
-                                                    <a href="contact.">contact us</a>
-                                                </li>
-                                                <li>
-                                                    <a href="my-account.">my account</a>
-                                                </li>
-                                                <li>
-                                                    <a href="login-register.">login / register</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">Food</a>
-                                            <ul>
-                                                <li><a href="#">Dogs Food</a>
-                                                    <ul>
-                                                        <li><a href="shop-page.">Grapes and Raisins</a></li>
-                                                        <li><a href="shop-page.">Carrots</a></li>
-                                                        <li><a href="shop-page.">Peanut Butter</a></li>
-                                                        <li><a href="shop-page.">Salmon fishs</a></li>
-                                                        <li><a href="shop-page.">Eggs</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="#">Cats Food</a>
-                                                    <ul>
-                                                        <li><a href="shop-page.">Meat</a></li>
-                                                        <li><a href="shop-page.">Fish</a></li>
-                                                        <li><a href="shop-page.">Eggs</a></li>
-                                                        <li><a href="shop-page.">Veggies</a></li>
-                                                        <li><a href="shop-page.">Cheese</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="#">Fishs Food</a>
-                                                    <ul>
-                                                        <li><a href="shop-page.">Rice</a></li>
-                                                        <li><a href="shop-page.">Veggies</a></li>
-                                                        <li><a href="shop-page.">Cheese</a></li>
-                                                        <li><a href="shop-page.">wheat bran</a></li>
-                                                        <li><a href="shop-page.">Cultivation</a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">blog</a>
-                                            <ul>
-                                                <li>
-                                                    <a href="blog.">blog page</a>
-                                                </li>
-                                                <li>
-                                                    <a href="blog-leftsidebar.">blog left sidebar</a>
-                                                </li>
-                                                <li>
-                                                    <a href="blog-details.">blog details</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="contact."> Contact us </a></li>
-                                    </ul>
-                                </nav>
                             </div>
                         </div>
                     </div>
